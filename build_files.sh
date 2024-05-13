848e5bd184ade9525d008ab5342a15cf4b3891ba
priyankaaurade1 <priyankaaurade365@gmail.com>
2024-05-13 15:52:45 +0530

    chnages in requirement.txt 4

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,23 @@
 
 echo "BUILD START"
+#!/bin/bash
+
+# Define the output directory
+OUTPUT_DIR="staticfiles_build"
+
+# Check if the output directory exists
+if [ ! -d "$OUTPUT_DIR" ]; then
+    # If the output directory does not exist, create it
+    mkdir -p "$OUTPUT_DIR"
+fi
+
+# Install Python dependencies
 python3.10.5 -m pip install -r requirements.txt
-python3.10.5 manage.py collectstatic --noinput --clear
+
+# Collect static files
+python3.10.5 manage.py collectstatic --no-input
+
+# Move static files to the output directory
+mv staticfiles/* "$OUTPUT_DIR/"
+
 echo "BUILD END"
